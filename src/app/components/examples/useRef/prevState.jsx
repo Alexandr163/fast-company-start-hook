80e6e7c894a2ce930bc18573b70e4e12f43d2049
Alexandr Morgunkov <shadow091@mail.ru>
2022-09-10 22:02:48 +0400

    finished lesson 1

--- src/app/components/examples/useRef/prevState.jsx
+++ src/app/components/examples/useRef/prevState.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
 const PrevStateExample = () => {
+    const prevState = useRef("");
+    const [otherState, setotherState] = useState("false");
+    const toggleOtherState = () => {
+        setotherState(prevState => prevState === "false" ? "true" : "false");
+    };
+    useEffect(() => {
+        prevState.current = otherState;
+    }, [otherState]);
+
     return (
         <CardWrapper>
             <SmallTitle>Предыдущее состояние</SmallTitle>
+            <Divider />
+            <p>prev state: {prevState.current}</p>
+            <p>current stste: {otherState}</p>
+            <button className="btn btn-primary" onClick={toggleOtherState}>
+                кнопка
+            </button>
         </CardWrapper>
     );
 };
--- src/app/components/examples/useRef/renderCount.jsx
+++ src/app/components/examples/useRef/renderCount.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
+
 const RenderCountExample = () => {
+    const renderCount = useRef(0);
+    const [otherState, setotherState] = useState(false);
+    const toggleOtherState = () => {
+        setotherState(!otherState);
+    };
+    useEffect(() => {
+        renderCount.current++;
+    });
     return (
         <CardWrapper>
             <SmallTitle>Подсчет количества рендеров</SmallTitle>
+            <Divider />
+            <p>render count: {renderCount.current}</p>
+            <button className="btn btn-primary" onClick={toggleOtherState}>
+                toggle other state
+            </button>
         </CardWrapper>
     );
 };
